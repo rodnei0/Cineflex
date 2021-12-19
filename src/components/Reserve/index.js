8acbc6e75615851bcf6b0595796485414656d6e0
rodnei94 <rodnei0@gmail.com>
2021-12-19 16:03:23 -0400

    corrigindo bugs

--- src/components/Reserve/index.js
+++ src/components/Reserve/index.js
@@ -3,6 +3,18 @@ import axios from 'axios'
 import './style.css';
 
 function ReserveSeat(name, cpf, selectedSeats, navigate) {
+    if (selectedSeats.length === 0) {
+        alert("Nenhum assento selecionado!");
+        return
+    }
+    if (name === "") {
+        alert("Nome não preenchido!");
+        return
+    }
+    if (cpf === "") {
+        alert("CPF não preenchido!");
+        return
+    }
 
     const promisse = axios.post("https://mock-api.driven.com.br/api/v4/cineflex/seats/book-many", {ids: selectedSeats, name: name, cpf: cpf});
     promisse.then(() => navigate("/success"));
